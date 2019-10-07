7937039ce3a756c67f645ed635e8a6707edfb110
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-07 21:38:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(string)
-  if string == string.
+  if string == string.upcase
+    
 end
 
 # Whatever you say to grandma, she should respond with
